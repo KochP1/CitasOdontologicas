e590e3e22d01aa4ddaee41315829e28c58592e56
kochp1 <juanandreskochp@gmail.com>
2025-05-15 17:19:02 -0400

    Añadir aplicaciones 'citas', 'doctors' y 'pacientes' a INSTALLED_APPS; crear migraciones iniciales para los modelos correspondientes.

--- servidor/App/App/settings.py
+++ servidor/App/App/settings.py
@@ -43,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'citas',
+    'doctors',
+    'pacientes',
 ]
 
 MIDDLEWARE = [
